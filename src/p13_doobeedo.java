8eae3ce8c2eca7df8d0c53986194c28b733a5efb
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-08-26 07:59:08 +0300

    song commit

---
+++ src/p13_doobeedo.java
@@ -0,0 +1,11 @@
+public class p13_doobeedo {
+    public static void main(String[] args) {
+        int x=1;
+        while (x<3){
+            System.out.print("DooBee");
+            x++;
+        }
+        System.out.print("Do");
+
+    }
+}
